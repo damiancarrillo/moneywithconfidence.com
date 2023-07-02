8425d58e747d8c9dd8b8e7d7ce1797e81301f179
Damian Carrillo <damiancarrillo@users.noreply.github.com>
2023-07-01 22:42:24 -0500

    Update contact form images

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -431,8 +431,8 @@
                 >
                   <div class="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                      alt=""
+                      src="/natasha-contact-3.jpg"
+                      alt="Natasha explaining budgeting techniques to a client"
                       class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div
@@ -445,8 +445,8 @@
                 >
                   <div class="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                      alt=""
+                      src="/natasha-contact-1.jpg"
+                      alt="Natasha laughing while looking at the camera"
                       class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div
@@ -455,8 +455,8 @@
                   </div>
                   <div class="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
-                      alt=""
+                      src="/natasha-contact-4.jpg"
+                      alt="Natasha walking down a bridge"
                       class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div
@@ -467,8 +467,8 @@
                 <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                   <div class="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
-                      alt=""
+                      src="/natasha-contact-2.jpg"
+                      alt="Natasha laughing"
                       class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div
@@ -477,8 +477,8 @@
                   </div>
                   <div class="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                      alt=""
+                      src="/natasha-contact-5.jpg"
+                      alt="Natasha logging into her computer"
                       class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div
