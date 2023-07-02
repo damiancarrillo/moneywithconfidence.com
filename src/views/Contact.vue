636ecd2fd8c9bdbb0613bd91f648b6d97e3b4fbe
Damian Carrillo <damiancarrillo@users.noreply.github.com>
2023-07-01 22:19:52 -0500

    Add basic validation

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -493,8 +493,8 @@
       </div>
     </main>
   </div>
-  <TransitionRoot as="template" :show="open">
-    <Dialog as="div" class="relative z-10" @close="open = false">
+  <TransitionRoot as="template" :show="openSuccessAlert">
+    <Dialog as="div" class="relative z-10" @close="openSuccessAlert = false">
       <TransitionChild
         as="template"
         enter="ease-out duration-300"
@@ -552,9 +552,159 @@
                 <button
                   type="button"
                   class="inline-flex w-full justify-center rounded-full bg-purple-600 hover:bg-purple-700 focus-visible:outline-purple-700 text-white shadow-purple-400/60 hover:shadow-purple-700/40 shadow-lg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 py-2"
-                  @click="open = false"
+                  @click="openSuccessAlert = false"
                 >
                   Done
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+  <TransitionRoot as="template" :show="openNameValidationAlert">
+    <Dialog
+      as="div"
+      class="relative z-10"
+      @close="openNameValidationAlert = false"
+    >
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+            >
+              <div class="sm:flex sm:items-start">
+                <div
+                  class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                >
+                  <ExclamationTriangleIcon
+                    class="h-6 w-6 text-red-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h3"
+                    class="text-base font-semibold leading-6 text-gray-900"
+                    >Invalid name</DialogTitle
+                  >
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">
+                      Please double check your name. It looks like you may have
+                      made a mistake while entering it.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  @click="openNameValidationAlert = false"
+                  ref="cancelButtonRef"
+                >
+                  Cancel
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+  <TransitionRoot as="template" :show="openEmailValidationAlert">
+    <Dialog
+      as="div"
+      class="relative z-10"
+      @close="openEmailValidationAlert = false"
+    >
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+            >
+              <div class="sm:flex sm:items-start">
+                <div
+                  class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                >
+                  <ExclamationTriangleIcon
+                    class="h-6 w-6 text-red-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h3"
+                    class="text-base font-semibold leading-6 text-gray-900"
+                    >Invalid email address</DialogTitle
+                  >
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">
+                      Please double check your email address. It looks like you
+                      may have made a mistake while entering it.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  @click="openEmailValidationAlert = false"
+                  ref="cancelButtonRef"
+                >
+                  Cancel
                 </button>
               </div>
             </DialogPanel>
@@ -574,7 +724,11 @@ import {
   ListboxOption,
   ListboxOptions,
 } from "@headlessui/vue";
-import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid";
+import {
+  CheckIcon,
+  ChevronUpDownIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/vue/20/solid";
 import { type ContactForm } from "../assets/js/Model";
 import {
   Dialog,
@@ -594,6 +748,7 @@ let mediaRequest = false;
 let otherRequest = false;
 let comment = "";
 
+const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 const locations = [
   { id: "friend-or-word-of-mouth", name: "Friend or word of mouth" },
   { id: "podcast", name: "Podcast" },
@@ -605,9 +760,23 @@ const locations = [
 ];
 
 const selected = ref(locations[0]);
-let open = ref(false);
+let openSuccessAlert = ref(false);
+let openNameValidationAlert = ref(false);
+let openEmailValidationAlert = ref(false);
 
 async function submitForm(event: Event) {
+  if (name.length <= 3) {
+    document.getElementById("name")?.focus();
+    openNameValidationAlert.value = true;
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    document.getElementById("email")?.focus();
+    openEmailValidationAlert.value = true;
+    return;
+  }
+
   let interests: string[] = [];
 
   if (oneOnOneFinancialCoaching) {
@@ -652,7 +821,7 @@ async function submitForm(event: Event) {
   });
 
   if (response.status === 201) {
-    open.value = true;
+    openSuccessAlert.value = true;
   }
 }
 </script>
