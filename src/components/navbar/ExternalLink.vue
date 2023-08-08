82edc5cb0b068b5f22a23e7b20297fae53bb8102
Damian Carrillo <damiancarrillo@users.noreply.github.com>
2023-08-08 09:08:32 -0500

    Fix mobile navbar highlight

--- src/components/navbar/ExternalLink.vue
+++ src/components/navbar/ExternalLink.vue
@@ -12,8 +12,8 @@ const props = defineProps<{
     target="_blank"
     :class="[
       Navbar.fullPathMatchesItemHref($route.fullPath, props.href)
-        ? 'bg-orange-100 hover:bg-orange-200 focus-visible:outline-orange-300 text-orange-600 shadow-sm'
-        : 'text-purple-600 hover:bg-purple-100 hover:shadow-purple-50 hover:shadow-md focus-visible:outline-purple-100',
+        ? 'bg-orange-100 hover:sm:bg-orange-200 focus-visible:sm:outline-orange-300 text-orange-600 sm:shadow-sm'
+        : 'text-purple-600 hover:sm:bg-purple-100 hover:sm:shadow-purple-50 hover:sm:shadow-md focus-visible:sm:outline-purple-100',
     ]"
     :aria-current="$route.fullPath === props.href ? 'page' : undefined"
     class="block px-5 py-2 text-base font-medium rounded-full focus-visible:outline-2 focus-visible:outline-offset-2"
